9d6084611ee6ccd130970b876d8110795fed7b60
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-20 01:35:10 +0100

    function

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './App.css';
 
 const user = {
-
+    firstName = 'Jokanola',
+    lastName = 'Adedipupo'
 }
+
 const element = <h1>Hello World Opor!!!</h1>;
 function App(user) {
   return (
