9e3603d4c81c3ad77690c1900c4621d62e034ffe
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-20 01:40:18 +0100

    function

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ function User(user) {
   return `user.firstName + '' + user.lastName` 
 }
 
+const element = <h1> H1 , ${User}</h1>;
 function App(user) {
   return (
     <div>
-     
+     {element};
      
     </div>
   );
